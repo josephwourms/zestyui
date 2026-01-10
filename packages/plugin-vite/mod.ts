ed55b24e13d029ac59841658464fb861124bddb0
Joseph Wourms <joseph.wourms@alertdriving.com>
2026-01-10 10:46:46 -0400

    create css-to-ts plugin to export minified css

--- packages/plugin-vite/mod.ts
+++ packages/plugin-vite/mod.ts
@@ -1,14 +1,14 @@
 import { Plugin } from "vite";
+import { styles } from "./dist/styles.ts";
 
 export function zestyui(): Plugin {
-  const cssContent = Deno.readTextFileSync(new URL("./dist/styles.css", import.meta.url));
   return {
     name: "zestyui",
     enforce: "pre",
 
     transform(src: string, id: string) {
       if (id.endsWith('.css') && src.includes('@import "zestyui";')) {
-        return src.replace('@import "zestyui";', cssContent);
+        return src.replace('@import "zestyui";', styles);
       }
       return null;
     }
